9ef6b3104ee71c377edbf18e7e574a520197ae34
Yegeun Kim <yegkim@c5r7s7.42seoul.kr>
2024-03-15 09:27:19 +0900

    change dir key < and >, left right move impl

--- key_press_handler.c
+++ key_press_handler.c
@@ -6,7 +6,7 @@
 /*   By: yegkim <yegkim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 13:41:21 by yegkim            #+#    #+#             */
-/*   Updated: 2024/03/14 15:53:09 by yegkim           ###   ########.fr       */
+/*   Updated: 2024/03/15 09:23:13 by yegkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 #include <math.h>
 
 void	draw_2D(t_info *info);
-void	raycast(t_info *info);
+void	draw_3D(t_info	*info);
 void	make_image_put_window(t_info *info, void (*draw_map)(t_info *info));
 
 int	go_front_back(int key, t_info *info)
@@ -31,20 +31,46 @@ int	go_front_back(int key, t_info *info)
 	if (key == K_W)
 	{
 		move_x = info->posX + info->dirX * info->moveSpeed;
-		move_y = info->posY + info->dirY * info->moveSpeed;
 		if (info->map->map[(int)(info->posY)][move_x] != 1)
 			info->posX += info->dirX * info->moveSpeed;
+		move_y = info->posY + info->dirY * info->moveSpeed;
 		if (info->map->map[move_y][(int)(info->posX)] != 1)
 			info->posY += info->dirY * info->moveSpeed;
 	}
 	if (key == K_S)
 	{
 		move_x = info->posX - info->dirX * info->moveSpeed;
-		move_y = info->posY - info->dirY * info->moveSpeed;
 		if (info->map->map[(int)(info->posY)][move_x] != 1)
 			info->posX -= info->dirX * info->moveSpeed;
+		move_y = info->posY - info->dirY * info->moveSpeed;
 		if (info->map->map[move_y][(int)(info->posX)] != 1)
 			info->posY -= info->dirY * info->moveSpeed;
+	}
+	return (0);
+}
+
+int	go_left_right(int key, t_info *info)
+{
+	int	move_x;
+	int	move_y;
+
+	if (key == K_A)
+	{
+		move_x = info->posX + info->dirY * info->moveSpeed;
+		if (info->map->map[(int)(info->posY)][move_x] != 1)
+			info->posX += info->dirY * info->moveSpeed;
+		move_y = info->posY - info->dirX * info->moveSpeed;
+		if (info->map->map[move_y][(int)(info->posX)] != 1)
+			info->posY -= info->dirX * info->moveSpeed;
+	}
+	if (key == K_D)
+	{
+		move_x = info->posX - info->dirY * info->moveSpeed;
+		if (info->map->map[(int)(info->posY)][move_x] != 1)
+			info->posX -= info->dirY * info->moveSpeed;
+		move_y = info->posY + info->dirX * info->moveSpeed;
+		if (info->map->map[move_y][(int)(info->posX)] != 1)
+			info->posY += info->dirX * info->moveSpeed;
 	}
 	return (0);
 }
@@ -54,7 +80,7 @@ int	change_dir(int key, t_info *info)
 	double	old_dir_x;
 	double	old_plane_x;
 
-	if (key == K_A)
+	if (key == K_AR_L)
 	{
 		old_dir_x = info->dirX;
 		info->dirX = info->dirX * cos(-info->rotSpeed) - info->dirY * sin(-info->rotSpeed);
@@ -63,7 +89,7 @@ int	change_dir(int key, t_info *info)
 		info->planeX = info->planeX * cos(-info->rotSpeed) - info->planeY * sin(-info->rotSpeed);
 		info->planeY = old_plane_x * sin(-info->rotSpeed) + info->planeY * cos(-info->rotSpeed);
 	}
-	if (key == K_D)
+	if (key == K_AR_R)
 	{
 		old_dir_x = info->dirX;
 		info->dirX = info->dirX * cos(info->rotSpeed) - info->dirY * sin(info->rotSpeed);
@@ -79,7 +105,9 @@ int	key_press_handler(int key, t_info *info)
 {
 	if (key == K_W || key == K_S)
 		go_front_back(key, info);
-	if (key == K_D || key == K_A)
+	if (key == K_A || key == K_D)
+		go_left_right(key, info);
+	if (key == K_AR_L || key == K_AR_R)
 		change_dir(key, info);
 	if (key == K_ESC)
 		exit(0);
