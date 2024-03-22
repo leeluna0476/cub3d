85166a7909a1a61903bd5dac2c76bede8ff5badf
leeluna0476 <leeluna0476@daum.net>
2024-03-22 17:50:52 +0900

    final commit

--- srcs/mandatory/main.c
+++ srcs/mandatory/main.c
@@ -6,7 +6,7 @@
 /*   By: yegkim <yegkim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 12:42:59 by yegkim            #+#    #+#             */
-/*   Updated: 2024/03/22 17:30:17 by seojilee         ###   ########.fr       */
+/*   Updated: 2024/03/22 17:50:43 by seojilee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,14 +30,8 @@ void	*null_guard(void *ptr)
 	return (ptr);
 }
 
-void	leaks(void)
-{
-	system("leaks -q cub3D");
-}
-
 int	main(int argc, char **argv)
 {
-	atexit(leaks);
 	if (argc != 2)
 		error_exit("argument number error\n");
 	exec_cub(argv);
